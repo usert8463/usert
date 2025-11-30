c0c64b9ebc72aa5b4ddfa07bb6bdd7f0eb0dca3d
usert8463 <usert8463@gmail.com>
2025-11-30 17:32:33 +0000

    Update message_upsert.js

--- Ovl_events/message_upsert.js
+++ Ovl_events/message_upsert.js
@@ -57,6 +57,7 @@ async function message_upsert(m, ovl) {
 
     const ms_org = ms.key.remoteJid;
     const id_Bot = decodeJid(ovl.user.id);
+    console.log(id_Bot);
     const id_Bot_N = id_Bot.split('@')[0];
 
     const verif_Groupe = ms_org.endsWith("@g.us");
