1bcd30190848f2a0fa929589f8caeed8c595286b
usert8463 <usert8463@gmail.com>
2025-11-30 12:10:45 +0000

    Update Ia.js

--- cmd/Ia.js
+++ cmd/Ia.js
@@ -16,11 +16,11 @@ ovlcmd(
         }
 
         const prompt = arg.join(" ");
-        const apiUrl = `https://api.shizo.top/ai/gpt?apikey=shizo&query=${encodeURIComponent(prompt)}`;
+        const apiUrl = `https://ab-chatgpt4o.abrahamdw882.workers.dev/?q=${encodeURIComponent(prompt)}`;
 
         try {
             const result = await axios.get(apiUrl);
-            const responseText = result.data?.msg || "Erreur de réponse de l\'API.";
+            const responseText = result.data?.data || "Erreur de réponse de l\'API.";
             return repondre(responseText);
         } catch (error) {
             console.error("Erreur GPT :", error);
@@ -44,18 +44,16 @@ ovlcmd(
         }
 
         try {
-            const prompt = encodeURIComponent(arg.join(" "));
-            const rep = await axios.get(`https://api.shizo.top/ai/imagine?apikey=shizo&prompt=${prompt}`, {
-                responseType: 'arraybuffer'
-            });
+            const prompt = arg.join(" ");
+        const apiUrl = `https://api-toxxic.zone.id/api/ai/ai4chat?prompt=${encodeURIComponent(prompt)}`;
 
-            const buffer = Buffer.from(rep.data);
+        try {
+            const result = await axios.get(apiUrl);
 
             return ovl.sendMessage(ms_org, {
-                image: buffer,
-                caption: "```Powered By OVL-MD```"
+                image: { url: result.data.data },
+                caption: "```Powered By OVL-MD-V2```"
             }, { quoted: ms });
-
         } catch (err) {
             console.error("Erreur DALLE :", err);
             return repondre("Erreur lors de la génération de l'image. Réessayez plus tard.");
@@ -68,63 +66,7 @@ ovlcmd(
         nom_cmd: "blackbox",
         classe: "IA",
         react: "🖤",
-        desc: "Utilise Blackbox pour répondre aux questions."
-    },
-    async (ms_org, ovl, cmd_options) => {
-        const { arg, ms, repondre } = cmd_options;
-
-        if (!arg.length) {
-            return repondre("Veuillez entrer un texte ou une question.");
-        }
-
-        const prompt = arg.join(" ");
-        const apiUrl = "https://nexra.aryahcr.cc/api/chat/complements";
-
-        try {
-            const result = await axios.post(apiUrl, {
-                messages: [{ role: "user", content: prompt }],
-                prompt: "Répondre à l'utilisateur.",
-                websearch: false,
-                stream: false,
-                markdown: false,
-                model: "blackbox"
-            }, {
-                headers: { 'Content-Type': 'application/json' }
-            });
-
-            const id = result.data.id;
-            let data = true;
-
-            while (data) {
-                const response = await axios.get(`https://nexra.aryahcr.cc/api/chat/task/${encodeURIComponent(id)}`);
-                switch (response.data.status) {
-                    case "pending":
-                        data = true;
-                        break;
-                    case "error":
-                        data = false;
-                        return repondre(" Une erreur est survenue lors du traitement de la requête.");
-                    case "completed":
-                        data = false;
-                        return repondre(response.data.message || "Aucune réponse générée.");
-                    case "not_found":
-                        data = false;
-                        return repondre("Tâche introuvable. Veuillez réessayer.");
-                }
-            }
-        } catch (error) {
-            console.error("Erreur Blackbox :", error);
-            return repondre("Une erreur est survenue lors de l\'appel à l\'API.");
-        }
-    }
-);
-
-ovlcmd(
-    {
-        nom_cmd: "lenna",
-        classe: "IA",
-        react: "🤖",
-        desc: "Utilise l'API Lenna pour générer des réponses."
+        desc: "Utilise l'API blackbox pour générer des réponses."
     },
     async (ms_org, ovl, cmd_options) => {
         const { arg, ms, repondre } = cmd_options;
@@ -134,14 +76,14 @@ ovlcmd(
         }
 
         const prompt = arg.join(" ");
-        const apiUrl = `https://fgsi.koyeb.app/api/ai/lenna?text=${encodeURIComponent(prompt)}`;
+        const apiUrl = `https://api-toxxic.zone.id/api/ai/blackbox?prompt=${encodeURIComponent(prompt)}`;
 
         try {
             const result = await axios.get(apiUrl);
-            const responseText = result.data?.message || "Erreur de réponse de l\'API.";
+            const responseText = result.data?.data || "Erreur de réponse de l\'API.";
             return repondre(responseText);
         } catch (error) {
-            console.error("Erreur Lenna :", error);
+            console.error("Erreur GPT :", error);
             return repondre("Une erreur est survenue lors de l\'appel à l\'API.");
         }
     }
@@ -162,7 +104,7 @@ ovlcmd(
         }
 
         const prompt = arg.join(" ");
-        const apiUrl = `https://fgsi.koyeb.app/api/ai/copilot?text=${encodeURIComponent(prompt)}`;
+        const apiUrl = `https://fgsi.koyeb.app/api/ai/copilot?apikey=fgsiapi-1e8a0e22-6d&text=${encodeURIComponent(prompt)}`;
 
         try {
             const result = await axios.get(apiUrl);
@@ -183,18 +125,18 @@ ovlcmd(
         desc: "Utilise l'API Gemini-Pro pour générer des réponses."
     },
     async (ms_org, ovl, cmd_options) => {
-        const { arg, ms, repondre } = cmd_options;
+        const { arg, ms, repondre, auteur_Message } = cmd_options;
 
         if (!arg.length) {
             return repondre("Veuillez entrer un prompt pour générer une réponse.");
         }
 
         const prompt = arg.join(" ");
-        const apiUrl = `https://bk9.fun/ai/gemini?q=${encodeURIComponent(prompt)}`;
+        const apiUrl = `https://fgsi.dpdns.org/api/ai/gemini?apikey=fgsiapi-1e8a0e22-6d&text=${encodeURIComponent(prompt)}&conversationId=${auteur_Message}`;
 
         try {
             const result = await axios.get(apiUrl);
-            const responseText = result.data?.BK9 || "Erreur de réponse de l\'API Gemini-Pro.";
+            const responseText = result.data?.data.answer || "Erreur de réponse de l\'API Gemini-Pro.";
             return repondre(responseText);
         } catch (error) {
             console.error("Erreur Gemini-Pro :", error);
@@ -231,7 +173,7 @@ ovlcmd(
     }
 );
 
-ovlcmd(
+/*ovlcmd(
     {
         nom_cmd: "bard",
         classe: "IA",
@@ -313,4 +255,4 @@ ovlcmd(
             return repondre("Une erreur est survenue lors de l\'appel à l\'API.");
         }
     }
-);
+);*/
