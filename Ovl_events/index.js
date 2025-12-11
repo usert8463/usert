1823f2a0fbe2f4bd1d9c31f303ff9e28bf899754
usert8463 <usert8463@gmail.com>
2025-12-11 12:17:00 +0000

    Update index.js

--- Ovl_events/index.js
+++ Ovl_events/index.js
@@ -3,6 +3,7 @@ const { dl_save_media_ms, recup_msg } = require('./autres_fonctions');
 module.exports = {
   message_upsert: require('./message_upsert'),
   group_participants_update: require('./group_participants_update'),
+  group_update: require('./group_update'),
   connection_update: require('./connection'),
   call: require('./call'),
   dl_save_media_ms,
