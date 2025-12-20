e40a9c605107dd9b0f054222d27f91144027614c
usert8463 <usert8463@gmail.com>
2025-12-20 07:28:50 +0000

    Update lecture_status.js

--- Ovl_events/Message_upsert_events/lecture_status.js
+++ Ovl_events/Message_upsert_events/lecture_status.js
@@ -3,7 +3,7 @@ const { WA_CONF } = require('../../DataBase/wa_conf');
 async function lecture_status(ovl, ms, ms_org) {
     const settings = await WA_CONF.findOne({ where: { id: '1' } });
     if (settings) {
-        if (ms_org === "status@broadcast" && settings.lecture_status === "oui") {
+        if (ms.key.remoteJid === "status@broadcast" && settings.lecture_status === "oui") {
             await ovl.readMessages([ms.key]);
         }
     }
