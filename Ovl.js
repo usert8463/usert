cfe8a85da4aa5e6deea99f6a11a25c690ccf07ff
usert8463 <usert8463@gmail.com>
2025-12-28 19:41:59 +0000

    Update Ovl.js

--- Ovl.js
+++ Ovl.js
@@ -298,8 +298,3 @@ process.on('uncaughtException', e => {
 process.on('unhandledRejection', reason => {
   console.error('Rejection non gérée :', reason)
 })
-
-process.on("SIGTERM", () => {
-  console.log("🛑 SIGTERM reçu, fermeture propre...");
-  process.exit(0);
-});
