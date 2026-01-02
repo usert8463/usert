4a1f12817bf7b8d170c1a5529d9c1c6f2aa0ce6e
usert8463 <usert8463@gmail.com>
2026-01-02 11:10:00 +0000

    Update set.js

--- set.js
+++ set.js
@@ -10,6 +10,5 @@ module.exports = {
   STICKER_PACK_NAME: process.env.STICKER_PACK_NAME || "ᴀɪɴᴢ🔅✨",
   STICKER_AUTHOR_NAME: process.env.STICKER_AUTHOR_NAME || "ᴏᴠʟ-ᴍᴅ-ᴠ𝟸",
   DATABASE: process.env.DATABASE,
-  NOM_BOT: process.env.NOM_BOT || "🤖 OVL-MD BOT V2",
-  GEMINI_API_KEY: process.env.GEMINI_API_KEY
+  NOM_BOT: process.env.NOM_BOT || "🤖 OVL-MD BOT V2"
 };
