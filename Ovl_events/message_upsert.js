c1801e747206dbb040feb77cb6aaba5d29d9dfbe
usert8463 <usert8463@gmail.com>
2026-01-11 15:49:36 +0000

    Update message_upsert.js

--- Ovl_events/message_upsert.js
+++ Ovl_events/message_upsert.js
@@ -38,10 +38,11 @@ async function isBanned(type, id) {
 
 async function message_upsert(m, ovl) {
   try {
-    console.log(m);
     if (m.type !== 'notify') return;
     const ms = m.messages?.[0];
     if (!ms?.message) return;
+    
+    console.log(ms);
     addMessage(ms.key.id, ms);
     const mtype = getContentType(ms.message);
     const texte = {
